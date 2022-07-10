9ba8f0f6b8cdcd78e25c92cb803f67343231181d
Ileana-B4 <ileana.tapia@uicui.edu.mx>
2022-07-09 21:58:45 -0500

    mi primer commit

---
+++ base-de-preguntas.js
@@ -0,0 +1,9 @@
+let baseDePreguntas = [
+    {
+    pregunta: "¿Cuales son los biolementos más abundantes en el cuerpo?",
+    imagen: "https://i.ibb.co/Syp937x/oligoelementos-sales-minerales.jpg",
+    respuesta: " Nitrogeno, Carbono, Hidrogeno y Oxigeno",
+    distractores: ["Oxigeno, Hidrogeno, Nitrogeno y Calcio","Carbono, Hidrogeno, Calcio y Oxigeno","Calcio, Hidrogeno, Potasio y Oxigeno"],
+    },
+]
+
