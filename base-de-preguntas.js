5257f449524478957cb5a5d817392ab703dea5b4
Ileana-B4 <ileana.tapia@uicui.edu.mx>
2022-07-10 15:19:10 -0500

    Actualizar

--- base-de-preguntas.js
+++ base-de-preguntas.js
@@ -1,6 +1,8 @@
 let baseDePreguntas = [
     {
-    pregunta: "¿Cuales son los biolementos más abundantes en el cuerpo?",
+    pregunta: "¿Cuales son los biolementos primarios en el cuerpo?",
+    ayuda: "Son los elementos mayoritarios de la materia viva (glúcidos, lípidos, proteínas y ácidos nucleicos), constituyen el 95% de la masa total y son indispensables para formar las biomoléculas.",
+    ayudaImg: "https://i.ibb.co/F49sWs4/26344-0896462001570340313.jpg",
     imagen: "https://i.ibb.co/Syp937x/oligoelementos-sales-minerales.jpg",
     respuesta: " Nitrogeno, Carbono, Hidrogeno y Oxigeno",
     distractores: ["Oxigeno, Hidrogeno, Nitrogeno y Calcio","Carbono, Hidrogeno, Calcio y Oxigeno","Calcio, Hidrogeno, Potasio y Oxigeno"],
---
+++ index.js
@@ -0,0 +1,48 @@
+cargarPregunta(0)
+
+function cargarPregunta(index){
+    objetoPregunta = baseDePreguntas[index]
+    opciones = [...objetoPregunta.distractores]
+    opciones.push(objetoPregunta.respuesta)
+    opciones.sort(()=>Math.random()-0.5);
+
+    for(let i = 0; i < 4; i++){
+        opciones.sort(() => Math.random() -0.5);
+    }
+
+    document.getElementById("pregunta").innerHTML = objetoPregunta.pregunta
+    document.getElementById("opcion-1").innerHTML = opciones[0]
+    document.getElementById("opcion-2").innerHTML = opciones[1]
+    document.getElementById("opcion-3").innerHTML = opciones[2]
+    document.getElementById("opcion-4").innerHTML = opciones[3]
+}
+
+async function seleccionaropcion(index){
+    let validez = opciones[index] == objetoPregunta.respuesta
+    if(validezRespuesta){
+        await Swal.fire({
+            title: "Respuesta correcta",
+            text: "La respuesta ha sido correcta",
+            icon: "success",
+        });
+    }else{
+        await Swal.Fire({
+            title: "Respuesta Incorrecta",
+            text: 'La repuesta ha sido ${objetoPregunta.respuesta}',
+            icon: "error", 
+        });  
+    }
+
+}
+
+
+function ayuda(){
+    Swal.fire({
+        title: '¡Pista!',
+        text: objetoPregunta.ayuda,
+        imageUrl: objetoPregunta.ayudaImg,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      }); 
+      
+}
